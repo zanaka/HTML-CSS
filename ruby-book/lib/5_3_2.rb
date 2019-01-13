7f1807939a905ea3bf4bb5dd0ec3c433c1bb8585
zanaka <e155752@ie.u-ryukyu.ac.jp>
2019-01-14 01:23:39 +0900

    Symbol

---
+++ ruby-book/lib/5_3_2.rb
@@ -0,0 +1,7 @@
+#文字列でのハッシュ
+currencies = { 'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'}
+puts currencies['japan']
+
+#シンボルでのハッシュ
+currencies = { :japan => 'yen', :us => 'dollar', :india => 'rupee'}
+puts currencies[:japan]
