1977724725f8ac2ef5dde743f427757cce5973c5
zanaka <e155752@ie.u-ryukyu.ac.jp>
2019-01-14 15:48:36 +0900

    Changed from strings to symbol

--- ruby-book/test/convert_length_test.rb
+++ ruby-book/test/convert_length_test.rb
@@ -3,8 +3,9 @@ require '../lib/convert_length'
 
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
-    assert_equal 39.37, convert_length(1, 'm', 'in')
-    assert_equal 0.38, convert_length(15, 'in', 'm')
-    assert_equal 10670.73, convert_length(35000, 'ft', 'm')
+    #キーワード引数で何を渡しているのか明確にする
+    assert_equal 39.37, convert_length(1, from: :m, to: :in)
+    assert_equal 0.38, convert_length(15, from: :in, to: :m)
+    assert_equal 10670.73, convert_length(35000, from: :ft, to: :m)
   end
 end
